55d41ac29911a2f51724c44f451f459e68712fef
Fuad9 <alfuad8548@gmail.com>
2020-12-09 21:13:09 +0600

    done

--- js/script.js
+++ js/script.js
@@ -1,69 +1,112 @@
-// /* ========== show menu ========== */
-// const showMenu = (toggleId, navId) => {
-//    const toggle = document.getElementById(toggleId),
-//       nav = document.getElementById(navId);
+/* ========== Show Scroll top ========== */
+const scrollTop = () => {
+   const scrollTop = document.getElementById("scroll-top");
+   if (this.scrollY >= 560) {
+      scrollTop.classList.add("show-scroll");
+   } else {
+      scrollTop.classList.remove("show-scroll");
+   }
+};
 
-//    if (toggle && nav) {
-//       toggle.addEventListener("click", () => {
-//          nav.classList.toggle("show-menu");
-//       });
-//    }
-// };
-// showMenu("nav-toggle-menu", "nav-menu-items");
+window.addEventListener("scroll", scrollTop);
 
-// /* ========== Remove Menu ========== */
-// const navLink = document.querySelectorAll(".nav-link");
+/* ========== Swipper ========== */
+var mySwiper = new Swiper(".swiper-container", {
+   // Optional parameters
+   direction: "horizontal",
+   spaceBetween: 55,
+   loop: true,
+   grabCursor: true,
 
-// const linkAction = () => {
-//    const navMenu = document.getElementById("nav-menu-items");
-//    navMenu.forEach((n) => n.addEventListener("click", linkAction));
-// };
+   // If we need pagination
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
 
-// /* ========== Scroll active link ========== */
-// const sections = document.querySelectorAll("section[id]");
+   breakpoints: {
+      500: {
+         slidesPerView: 1,
+      },
 
-// const scrollActive = () => {
-//    const scrollY = window.pageYOffset;
+      1024: {
+         slidesPerView: 1,
+      },
+   },
+});
 
-//    sections.forEach((curr) => {
-//       const sectionHeight = current.offsetHeight;
-//       const sectionTop = current.offsetTop - 50;
-//       sectionId = curr.getAttribute("id");
+/* ========== Gsap Animation ========== */
 
-//       if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-//          document
-//             .querySelector(".nav-items a[href*=' + sectionId + ']")
-//             .classList.add("active-link");
-//       } else {
-//          document
-//             .querySelector(".nav-items a[href*=' + sectionId + ']")
-//             .classList.remove("active-link");
-//       }
-//    });
-// };
+/* ========== Navbar ========== */
+gsap.from(".nav-item, .nav-logo", {
+   opacity: 0,
+   duration: 2,
+   delay: 0.5,
+   y: -100,
+   ease: "expo-out",
+   stagger: 0.6,
+});
 
-// window.addEventListener("scroll", scrollActive);
+/* ========== Header ========== */
+gsap.from(".embed-responsive-video", { opacity: 0, duration: 2, delay: 0.7, x: 60 });
 
-// /* ========== Header Background change ========== */
-// const scrollHeader = () => {
-//    const header = document.getElementById("header");
-//    if (this.scrollY >= 200) {
-//       header.classList.add("scroll-header");
-//    } else {
-//       header.classList.remove("scroll-header");
-//    }
-// };
+gsap.from(".header-text, .header-button", {
+   opacity: 0,
+   duration: 3,
+   delay: 0.8,
+   y: 100,
+   ease: "expo-out",
+   stagger: 0.8,
+});
 
-// window.addEventListener("scroll", scrollHeader);
+/* ========== testimonials ========== */
+gsap.from(".testimonials-item", {
+   opacity: 0,
+   duration: 4,
+   delay: 1,
+   y: 100,
+   ease: "expo-out",
+   stagger: 0.6,
+});
 
-// /* ========== Show Scroll top ========== */
-// const scrollTop = () => {
-//    const scrollTop = document.getElementById("scroll-top");
-//    if (this.scrollY >= 560) {
-//       scrollTop.classList.add("show-scroll");
-//    } else {
-//       scrollTop.classList.remove("show-scroll");
-//    }
-// };
+gsap.registerPlugin(ScrollTrigger);
 
-// window.addEventListener("scroll", scrollTop);
+gsap.to(".testimonials-item", {
+   scrollTrigger: {
+      trigger: ".header-main",
+      toggleActions: "restart none none none",
+   },
+   x: 100,
+   rotation: 360,
+   duration: 5,
+});
+
+/* ========== Spciality ========== */
+gsap.from(".speciality-item-left", { opacity: 0, duration: 6, delay: 1.9, x: -100 });
+
+gsap.from(".speciality-item-right", { opacity: 0, duration: 7, delay: 2, x: 100 });
+
+/* ========== Contact ========== */
+gsap.from("#contact-item-one", { opacity: 0, duration: 8, delay: 2.5, x: -150 });
+gsap.from("#contact-item-two", { opacity: 0, duration: 9, delay: 2.6, y: -150 });
+gsap.from("#contact-item-three", { opacity: 0, duration: 10, delay: 2.7, x: 150 });
+
+/* ========== Live Class ========== */
+gsap.from("#live-class-item-left", { opacity: 0, duration: 11, delay: 2.9, y: -150 });
+gsap.from("#live-class-item-right", { opacity: 0, duration: 12, delay: 3, y: 150 });
+
+/* ========== Courses, Books, Boards ========== */
+gsap.from(".card", {
+   opacity: 0,
+   duration: 8,
+   delay: 2.5,
+   y: 100,
+   ease: "expo-out",
+   stagger: 0.8,
+});
+
+/* ========== Tutor ========== */
+gsap.from("#tutor-one", { opacity: 0, duration: 10, delay: 6, y: -150 });
+gsap.from("#tutor-two", { opacity: 0, duration: 10, delay: 6.1, y: 150 });
+gsap.from("#tutor-three", { opacity: 0, duration: 10, delay: 6.2, x: -150 });
+gsap.from("#tutor-four", { opacity: 0, duration: 10, delay: 6.3, x: 150 });
